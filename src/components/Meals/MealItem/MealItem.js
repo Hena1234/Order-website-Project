9716b719bdab6267ec65d89653ed73aab122edeb
Hyeon(Hena) Park <93799006+Hena1234@users.noreply.github.com>
2023-05-24 15:53:14 -0400

    Add and remove items from Cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,18 @@
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
-
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 const MealItem = (props) => {
+  const CartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+  const addToCartHandler = (amount) => {
+    CartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +21,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
